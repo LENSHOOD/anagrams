e24ee7e062e60917015365ecd1b0e2c7e69065f1
zhangxuhai <xuhai.zhang@thoughtworks.com>
2019-10-17 22:31:51 +0800

    feat: add basic method for compare two string

--- src/main/java/zxh/demo/anagram/AnagramFinder.java
+++ src/main/java/zxh/demo/anagram/AnagramFinder.java
@@ -2,6 +2,8 @@ package zxh.demo.anagram;
 
 import zxh.demo.anagram.domain.AnagramResult;
 
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 /**
@@ -22,5 +24,22 @@ public class AnagramFinder {
                 .setCount(inputArray.length)
                 .longestSetLength(longestSetLength)
                 .build();
+    }
+
+    public boolean compareCharacters(String s1, String s2) {
+        if (Objects.isNull(s1) || Objects.isNull(s2)) {
+            return false;
+        }
+
+        char[] s1c = s1.toCharArray();
+        char[] s2c = s2.toCharArray();
+
+        if (s1c.length != s2c.length) {
+            return false;
+        }
+
+        Arrays.sort(s1c);
+        Arrays.sort(s2c);
+        return new String(s1c).equalsIgnoreCase(new String(s2c));
     }
 }
--- src/test/java/zxh/demo/anagram/AnagramFinderTest.java
+++ src/test/java/zxh/demo/anagram/AnagramFinderTest.java
@@ -3,26 +3,9 @@ package zxh.demo.anagram;
 import org.junit.Test;
 import zxh.demo.anagram.domain.AnagramResult;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class AnagramFinderTest {
-
-    @Test
-    public void validate_input_array_then_output_same_array() {
-        String[] givenArray = new String[] {
-                "I'm ZXH", "I love coding", "hurrah"
-        };
-
-        AnagramFinder anagramFinder = new AnagramFinder();
-        AnagramResult anagramResult = anagramFinder.find(givenArray);
-
-        assertEquals(givenArray[0], anagramResult.getAnagramArray()[0]);
-        assertEquals(givenArray[1], anagramResult.getAnagramArray()[1]);
-        assertEquals(givenArray[2], anagramResult.getAnagramArray()[2]);
-
-        assertEquals(3, anagramResult.getSetCount());
-        assertEquals(3, anagramResult.getLongestSetLength());
-    }
 
     @Test
     public void validate_one_word_input_array_then_output_same_array() {
@@ -55,4 +38,17 @@ public class AnagramFinderTest {
         assertEquals(0, anagramResult.getLongestSetLength());
     }
 
+    @Test
+    public void validate_compareCharacters() {
+        AnagramFinder anagramFinder = new AnagramFinder();
+
+        assertTrue(anagramFinder.compareCharacters("kinship", "pinkish"));
+        assertTrue(anagramFinder.compareCharacters("boaster", "boaters"));
+        assertTrue(anagramFinder.compareCharacters("ABC", "CAB"));
+
+        assertFalse(anagramFinder.compareCharacters("kinship", "refresh"));
+        assertFalse(anagramFinder.compareCharacters("ABC", "AB"));
+        assertFalse(anagramFinder.compareCharacters("123", ""));
+        assertFalse(anagramFinder.compareCharacters(null, null));
+    }
 }
